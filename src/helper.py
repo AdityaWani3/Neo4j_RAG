96996e4fb1dac6dfd17f72316a486692b59938e4
AdityaWani3 <adityawani333@gmail.com>
2025-06-29 11:15:30 +0530

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -39,7 +39,7 @@ def llm_model_object(user_text):
     
     genai.configure(api_key=GOOGLE_API_KEY)
     
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-2.0-pro-exp')
     
     response=model.generate_content(user_text)
     
